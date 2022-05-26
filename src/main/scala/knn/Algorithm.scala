c0cb0e863a9dd82445ff29a496fd4c81c40c28b4
Roly Espinosa Alvarez <rolyespinosa99@gmail.com>
2022-05-26 17:18:25 -0400

    Auxiliary Class Created

--- src/main/scala/knn/Algorithm.scala
+++ src/main/scala/knn/Algorithm.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{StringType, StructField}
 import org.apache.spark.storage.StorageLevel
+import AuxiliaryClass._
 
 object Algorithm {
 
@@ -37,7 +38,6 @@ object Algorithm {
         println("              PARSEANDO TUPLAS")
         println("**********************************************")
         val ds = data.map { row => parseTupla(row, spark, ID) }
-
         println("Ejecutando Fase1")
 
         println("**********************************************")
@@ -488,80 +488,5 @@ object Algorithm {
         TuplaBanco(id, mes, importe)
 
     }
-
-    /** Tupla es una estructura que se utiliza en los DataSets
-     *
-     * @constructor crea una nueva Tupla con su identificador y un arreglo compuesto por los valores de los atributos de la tupla.
-     * @param id      es un Long que representa el identificador único de la Tupla
-     * @param valores es un arreglo de Double que representa los valores de los atributos de la tupla
-     */
-    case class Tupla(id: String, valores: Array[Double]) extends Serializable
-
-    /** TuplaBanco es una estructura que se utiliza en los DataSets
-     *
-     * @constructor crea una nueva tupla con su identificador, el dia del mes y el importe.
-     * @param id      es un Long que representa el identificador único de la Tupla
-     * @param mes     es un entero que representa el dia del mes
-     * @param importe es un Double que representa el importe
-     */
-    case class TuplaBanco(id: Long, mes: Int, importe: Double) extends Serializable
-
-    /** TuplaFase1 es una estructura que se utiliza en el DataSet que se obtiene de la función fase1
-     *
-     * @constructor crea una nueva tupla con su identificador único, un arreglo compuesto por los valores de los atributos de la tupla  y el  índice de anomalías.
-     * @param id      es un Long que representa el identificador único de la tupla
-     * @param valores es un arreglo de Double que representa los valores de los atributos de la tupla
-     * @param ia      es un Double que representa el índice de anomalía de la tupla
-     */
-    case class TuplaFase1(id: String, valores: Seq[Double], ia: Double) extends Serializable
-
-    /** TuplaFase1Banco es una estructura que se utiliza en el DataSet que se obtiene de la función fase1Banco
-     *
-     * @constructor crea una nueva tupla con su identificador único, el dia del mes, el importe y el  índice de anomalías.
-     * @param id      es un Long que representa el identificador único de la tupla
-     * @param mes     es un entero que representa el dia del mes
-     * @param importe es un Double que representa el importe
-     * @param ia      es un Double que representa el índice de anomalía de la tupla
-     */
-    case class TuplaFase1Banco(id: Long, mes: Int, importe: Double, ia: Double) extends Serializable
-
-    /** TuplaFase2 es una estructura que se utiliza en el DataSet en la función fase2
-     *
-     * @constructor crea una nueva tupla con su identificador único, un arreglo compuesto por los valores de los atributos de la tupla  y el  índice de anomalías.
-     * @param id         es un Long que representa el identificador único de la tupla
-     * @param valores    es un arreglo de Double que representa los valores de los atributos de la tupla
-     * @param distancias es un arreglo de Double que representa las distancias de los k vecinos cercanos de la tupla
-     */
-    case class TuplaFase2(id: String, valores: Seq[Double], distancias: Seq[Double]) extends Serializable
-
-    /** TuplaFase2 es una estructura que se utiliza en el DataSet en la función fase2Banco
-     *
-     * @constructor crea una nueva tupla con su identificador único, un arreglo compuesto por los valores de los atributos de la tupla  y el  índice de anomalías.
-     * @param id         es un Long que representa el identificador único de la tupla
-     * @param mes        es un entero que representa el dia del mes
-     * @param importe    es un Double que representa el importe
-     * @param distancias es un arreglo de Double que representa las distancias de los k vecinos cercanos de la tupla
-     */
-    case class TuplaFase2Banco(id: Long, mes: Int, importe: Double, distancias: Seq[Double]) extends Serializable
-
-    /** Resultado es una estructura que se utiliza en el DataSet que retorna la función exce
-     *
-     * @constructor crea una nueva tupla con su identificador único, un arreglo compuesto por los valores de los atributos de la tupla  y el  índice de anomalías.
-     * @param ID es un Long que representa el identificador único de la tupla
-     * @param ia es un Double que representa el índice de anomalía de la tupla
-     */
-    case class Resultado(ID: String, ia: Double, data: Seq[Double]) extends Serializable
-    case class Clasificacion(id: String, ia: Double, data: Seq[Double], tipo: String) extends Serializable
-
-    /** ResultadoBanco es una estructura que se utiliza en el DataSet que retorna la función exce
-     *
-     * @constructor crea una nueva tupla con su identificador único, un arreglo compuesto por los valores de los atributos de la tupla  y el  índice de anomalías.
-     * @param id      es un Long que representa el identificador único de la tupla
-     * @param mes     es un entero que representa el dia del mes
-     * @param importe es un Double que representa el importe
-     * @param ia      es un Double que representa el índice de anomalía de la tupla
-     */
-    case class ResultadoBanco(id: Long, mes: Int, importe: Double, ia: Double) extends Serializable
-
 
 }
--- src/main/scala/knn/Main.scala
+++ src/main/scala/knn/Main.scala
@@ -1,12 +1,14 @@
 package knn
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 
 object Main {
 
     def main(args: Array[String]): Unit = {
 
-        val db = "satimage_id.csv"
+        val db = "satimage_id"
 
         val spark = SparkSession
           .builder
@@ -14,11 +16,21 @@ object Main {
           .config("spark.master", "local")
           .getOrCreate()
 
-        val df = spark.read.options(Map("delimiter"->",", "header"->"true")).csv("db/"+db)
+        var df = spark.read.options(Map("delimiter"->",", "header"->"true")).csv("db/"+db+".csv")
+        val df_temporary = spark.read.options(Map("delimiter"->",", "header"->"true")).csv("results/"+db)
+
         val k = 2
         val p = 0.01
 
         val df_classified = Algorithm.train(df, spark, k, p)
+        df_classified.withColumn("data", stringify(df_classified.col("data")))
+          .write.mode(SaveMode.Overwrite).option("header", "true").csv("results/"+db)
+
+        def stringify(c: Column) = concat(lit("["), concat_ws(",",c), lit("]"))
+
+
+
+
     }
 
 }
